fea47f48d9824d924be69f071ee12e9fbca589a5
tanmaypardeshi <tanmaypardeshi@gmail.com>
2020-10-08 00:59:32 +0530

    Added post tasks

--- backend/cheer/views.py
+++ backend/cheer/views.py
@@ -27,6 +27,28 @@ class TaskView(generics.GenericAPIView):
                 'message': e.__str__()
             }, status=status.HTTP_400_BAD_REQUEST)
 
+    def post(self, request, *args, **kwargs):
+        try:
+            data = request.data
+            user = request.user
+            subtasks = data.pop('subtasks')
+            task = Task(user=user, task=data['task'])
+            taken = Taken(user=user, task=task)
+            task.save()
+            taken.save()
+            for subtask in subtasks:
+                sub = SubTask(task=task, title=subtask['title'])
+                sub.save()
+            return Response({
+                'success': True,
+                'message': 'Added new task'
+            }, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({
+                'success': True,
+                'message': e.__str__()
+            }, status=status.HTTP_400_BAD_REQUEST)
+
 
 class UserTaskView(generics.GenericAPIView):
     permission_classes = (IsAuthenticated, )
@@ -58,7 +80,7 @@ class SingleTaskView(APIView):
             task_list = get_tasks(task, request)
             return Response({
                 'success': True,
-                'message': f"Fetched task id {kwargs['id']} successfully",
+                'message': f"Fetched task ID {kwargs['id']} successfully",
                 'task': task_list
             }, status=status.HTTP_200_OK)
         except Exception as e:
@@ -70,34 +92,36 @@ class SingleTaskView(APIView):
 
 def get_tasks(tasks, request):
     tasks_list = []
+    subtask_list = []
     objects = {}
-    if tasks == 1:
+    sub_objects = {}
+    for task in tasks:
+        completed = 0
+        total = 0
+        subtasks = SubTask.objects.filter(task=task.id)
+        for subtask in subtasks:
+            sub_objects['title'] = subtask.title
+            if subtask.is_subtask:
+                completed = completed + 1
+            subtask_list.append(sub_objects)
+            total = total + 1
+            sub_objects = {}
         try:
-            Taken.objects.get(task=tasks, user=request.user)
+            Taken.objects.get(task=task, user=request.user)
             is_taken = True
         except Taken.DoesNotExist:
             is_taken = False
-        objects['id'] = tasks.id
-        objects['first_name'] = tasks.user.first_name
-        objects['last_name'] = tasks.user.last_name
-        objects['task'] = tasks.task
+        objects['id'] = task.id
+        objects['first_name'] = task.user.first_name
+        objects['last_name'] = task.user.last_name
+        objects['task'] = task.task
         objects['is_taken'] = is_taken
-        return objects
-
-    else:
-        for task in tasks:
-            try:
-                Taken.objects.get(task=task, user=request.user)
-                is_taken = True
-            except Taken.DoesNotExist:
-                is_taken = False
-            objects['id'] = task.id
-            objects['first_name'] = task.user.first_name
-            objects['last_name'] = task.user.last_name
-            objects['task'] = task.task
-            objects['is_taken'] = is_taken
-            tasks_list.append(objects)
-            objects = {}
-        return tasks_list
+        if is_taken:
+            objects['progress'] = (completed / total) * 100
+        objects['sub_tasks'] = subtask_list
+        tasks_list.append(objects)
+        objects = {}
+        subtask_list = []
+    return tasks_list
 
 
