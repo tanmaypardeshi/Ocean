1497d3e3540177475061ab0e4c09cf4ba6eb3721
tanmaypardeshi <tanmaypardeshi@gmail.com>
2020-10-26 15:17:47 +0530

    Added pagination in cheer squad

--- backend/cheer/urls.py
+++ backend/cheer/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import (TaskView, UserTaskView, SingleTaskView,
+from .views import (GetTaskView, PostTaskView, UserTaskView, SingleTaskView,
                     TakeTaskView, DeleteTaskView, UpdateProgressView)
 
 urlpatterns = [
-    path('gettasks/', TaskView.as_view(), name='get-tasks'),
+    path('gettasks/<int:id>/', GetTaskView.as_view(), name='get-tasks'),
+    path('posttask/', PostTaskView.as_view(), name='post-tasks'),
     path('mytasks/', UserTaskView.as_view(), name='user-tasks'),
     path('follow/<int:id>/', TakeTaskView.as_view(), name='follow'),
     path('unfollow/<int:id>/', DeleteTaskView.as_view(), name='unfollow'),
